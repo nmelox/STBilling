f4ad899d4e7fa5d4dd6da77668083e8e17d6f593
Nahuel Adrian Melo <nmelo@belatrix.com>
2018-01-23 18:03:42 -0300

    Add Data.json, refactor LoginPage.py

--- Login.py
+++ Login.py
@@ -1,8 +1,5 @@
 import unittest
-import datetime
 from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.support.ui import Select
 from PageObjects.LoginPage import LoginPage
 
 class LoginSTBilling (unittest.TestCase):
@@ -12,12 +9,11 @@ class LoginSTBilling (unittest.TestCase):
     def test_login (self):
         driver = self.driver
         driver.get("http://tl-stbilling12:8080/")
-        self.assertIn("ShopperTrak Billing", driver.title)
+        #self.assertIn("ShopperTrak Billing", driver.title)
         login = LoginPage(driver)
-        login.login()
-        driver.get_screenshot_as_file('C:\Automation\STBilling\Screenshots\Login_' + datetime.datetime.now().strftime("%Y-%m-%d_%H-%M") + '.png')
+        login.loginUser()
+
 
     def tearDown(self):
-
         self.driver.close()
         self.driver.quit()
--- PageObjects/LoginPage.py
+++ PageObjects/LoginPage.py
@@ -1,31 +1,37 @@
 from selenium.webdriver.support.select import Select
+from selenium.webdriver.common.by import  By
+import json
+import datetime
 
 
-class LoginPage (object):
+class LoginPage ():
 
-    userNameField = ('//*[@id="root"]/div/div[2]/div/div/form/table/tbody/tr[1]/td[2]/input')
-    passwordField = ('//*[@id="root"]/div/div[2]/div/div/form/table/tbody/tr[2]/td[2]/input')
-    businessUnitSelector = ('//*[@id="root"]/div/div[2]/div/div/form/table/tbody/tr[3]/td[2]/select')
-    businessUnitText = 'ShopperTrak Ltd (UK)'
-    businessUnitId = '300000003692534'
-    submitButton = ('//*[@id="root"]/div/div[2]/div/div/form/button')
-    userName = "nmelo@shoppertrak.com"
-    password = "test"
+    userNameField = (By.XPATH, '//*[@id="root"]/div/div[2]/div/div/form/table/tbody/tr[1]/td[2]/input')
+    passwordField = (By.XPATH, '//*[@id="root"]/div/div[2]/div/div/form/table/tbody/tr[2]/td[2]/input')
+    businessUnitSelector = (By.XPATH, '//*[@id="root"]/div/div[2]/div/div/form/table/tbody/tr[3]/td[2]/select')
+    submitButton = (By.XPATH, '//*[@id="root"]/div/div[2]/div/div/form/button')
 
     def __init__(self, driver):
+        with open('Data.json') as data_file:
+            data = json.load(data_file)
         self.driver = driver
+        self.userName = data["User"]["userName"]
+        self.password = data["User"]["password"]
+        self.businessUnit = data["BusinessUnit"]["Name"]
 
 
     def enterUserName (self):
-        self.driver.find_element_by_xpath(self.userNameField).send_keys(self.userName)
+        self.driver.find_element(*self.userNameField).send_keys(self.userName)
 
     def enterPassword (self):
-        self.driver.find_element_by_xpath(self.passwordField).send_keys(self.password)
+        self.driver.find_element(*self.passwordField).send_keys(self.password )
 
     def selectBusinessUnit(self):
-        Select(self.driver.find_element_by_xpath(self.businessUnitSelector)).select_by_visible_text(self.businessUnitText)
+        Select(self.driver.find_element(*self.businessUnitSelector)).select_by_visible_text(self.businessUnit)
 
-    def login(self):
+    def loginUser(self):
         self.enterUserName()
         self.enterPassword()
         self.selectBusinessUnit()
+        self.driver.get_screenshot_as_file(
+            'C:\Automation\STBilling\Screenshots\Login_' + datetime.datetime.now().strftime("%Y-%m-%d_%H-%M") + '.png')
